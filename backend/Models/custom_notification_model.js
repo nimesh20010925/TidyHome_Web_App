31d4adb1528f3a85648238979037e2c5728dcc4d
Tharusha Rashmika Sandakelum <103655595+Tharusha200219@users.noreply.github.com>
2025-03-11 23:12:56 +0530

    add custom notification model with schema definition

---
+++ backend/Models/custom_notification_model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const customNotificationSchema = new mongoose.Schema(
+  {
+    notification_title: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    notification_type: {
+      type: String,
+      required: true,
+    },
+    notification_type: {
+      type: String,
+      required: true,
+    },
+    assign_to: {
+      type: String,
+      required: true,
+    },
+    repeat_notification: {
+      type: String,
+      required: true,
+    },
+    send_notification_via: {
+      type: String,
+      required: true,
+    },
+    priority_level: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    Actions: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const customNotification = mongoose.model(
+  "customNotifications",
+  customNotificationSchema
+);
